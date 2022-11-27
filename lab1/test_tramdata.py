f0bee66440d4bb52d5742d1558a34ea376a27e40
tonygromer <116736431+tonygromer@users.noreply.github.com>
2022-11-27 13:43:19 +0100

    added test lines exist

--- lab1/test_tramdata.py
+++ lab1/test_tramdata.py
@@ -2,6 +2,9 @@ import unittest
 from tramdata import *
 
 TRAM_FILE = './tramnetwork.json'
+ORG_LINES = './data/tramlines.txt'
+ORG_STOPS = './data/tramstops.json'
+
 
 class TestTramData(unittest.TestCase):
 
@@ -10,6 +13,12 @@ class TestTramData(unittest.TestCase):
             tramdict = json.loads(trams.read())
             self.stopdict = tramdict['stops']
             self.linedict = tramdict['lines']
+        
+        with open(ORG_LINES) as lines:
+            self.tram_lines = lines.read()
+        
+        with open(ORG_STOPS) as stops:
+            self.tram_stops = json.loads(stops.read())
 
     def test_stops_exist(self):
         stopset = {stop for line in self.linedict for stop in self.linedict[line]}
@@ -17,6 +26,19 @@ class TestTramData(unittest.TestCase):
             self.assertIn(stop, self.stopdict, msg = stop + ' not in stopdict')
 
     # add your own tests here
+    # ^ above test_stops_exists() test functions 1,2,3.
+    def test_lines_exist_from_original(self):
+        org_lines = {line for line in self.tram_lines if line[0].isdigit()}
+        for line in org_lines:
+            self.assertIn(line, self.tram_lines, msg = line + ' does not exist in original file')
+
+
+    def test_stops_extist_from_original(self):
+        org_stops = {}
+        return
+
+    def test_feasible_distance(self):
+        return
 
 
 if __name__ == '__main__':
