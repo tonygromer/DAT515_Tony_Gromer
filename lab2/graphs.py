4bd15da680609b722113e778124f7bd0b65ee7df
tonygromer <gromer@chalmers.se>
2022-12-02 14:19:27 +0100

    graphs and trams with classes (empty)

---
+++ lab2/graphs.py
@@ -0,0 +1,12 @@
+class WeightedGraph:
+    print('a')
+
+class Graph:
+    print('a')
+
+# in graphs.py
+def dijkstra(graph, source, cost=lambda u,v: 1):
+    print('a')
+
+def visualize(graph, view='dot', name='mygraph', nodecolors={}, engine='dot'):
+    print('a')
---
+++ lab2/trams.py
@@ -0,0 +1,12 @@
+class TramStop:
+    print('a')
+
+class TramLine:
+    print('a')
+
+class TramNetwork:
+    print('a')
+
+# in trams.py
+def readTramNetwork(file='tramnetwork.json'):
+    print('a')
