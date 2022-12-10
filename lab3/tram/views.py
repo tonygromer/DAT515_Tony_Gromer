ad7fca00e31d633ce11afe949e1d194264c2d50b
tonygromer <gromer@chalmers.se>
2022-12-10 20:32:42 +0100

    added wiki map

--- lab3/tram/views.py
+++ lab3/tram/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .forms import RouteForm
 
-# Create your views here.
+def tram_net(request):
+    return render(request, 'tram/home.html', {})
+
+def find_route(request):
+    form = RouteForm()
+    return render(request, 'tram/find_route.html', {'form': form})
